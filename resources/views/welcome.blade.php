11971e7da77058d0ba98483b6f8a9db2d02d7155
NarukiHayshi <naruking_13@ezweb.ne.jp>
2019-09-21 09:11:59 +0000

    set timezone

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Larabl
                 </div>
 
                 <div class="links">
