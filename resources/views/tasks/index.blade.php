6e565e6d132c8610c540e36703ec045ec5d8c777
NarukiHayshi <naruking_13@ezweb.ne.jp>
2019-09-27 07:50:57 +0000

    Procfile

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -24,6 +24,7 @@
             </tbody>
         </table>
     @endif
+    {{ $tasks->links('pagination::bootstrap-4') }} 
     {!! link_to_route('tasks.create', '新規メッセージの投稿', [],['clas
     s' => 'btn btn-primary']) !!}
 
